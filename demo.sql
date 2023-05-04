3c69839a7291ca4bc65a2488a63aa875fc35346d
AndresPC <andrescarabajal@gmail.com>
2023-05-03 22:32:10 -0300

    sql

--- demo.sql
+++ demo.sql
@@ -1,3 +1,5 @@
-select * from student
-INNER JOIN largo
-on student.id = largo.id;
+select * from student;
+insert into student (id, name, age) values (5, "Louis", 35);
+delete from student where Name="Andres";
+delete from student where Age=20;
+SET SQL_SAFE_UPDATES = 0;
