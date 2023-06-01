fd5dda7b784794c8a231f717c09a00000fd24bfa
AndresPC <andrescarabajal@gmail.com>
2023-06-01 19:02:59 -0300

    sql

--- demo.sql
+++ demo.sql
@@ -1,5 +1,7 @@
 select * from student;
-insert into student (id, name, age) values (5, "Louis", 35);
+insert into student (id, name, age) values (4, "Man", 19);
 delete from student where Name="Andres";
-delete from student where Age=20;
+delete from student where Age=19;
 SET SQL_SAFE_UPDATES = 0;
+select name from student where age = (select MAX(age) from student);
+update student set age=39 where age=79;
